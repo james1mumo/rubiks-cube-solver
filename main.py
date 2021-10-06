a57d058e999c2b36fd010d3a8395783e545dd23b
James Mumo <james1mumo@gmail.com>
2021-10-06 12:04:31 +0300

    Can perform all rubik's cube basic moves.

--- main.py
+++ main.py
@@ -196,11 +196,24 @@ def get_left_face(front_face):
 
 
 def set_front_face(front_face):
+    global current_front_face
+    if current_front_face == front_face:
+        return
 
-    old_front_face = copy.deepcopy(current_front_face)
+    # red[4] --> right of red is --> green[2] and so on
+    # faces = r, g, o, b
+    faces = [4, 2, 5, 3]
+
+    rotations = faces.index(front_face) - faces.index(current_front_face)
+    if rotations < 0:
+        rotations += 4
+    upper_face = np.array_split(cube[0], 3)
+    bottom_face = np.array_split(cube[1], 3)
+
+    cube[0] = list(np.concatenate(np.rot90(upper_face, 4 - rotations)))  # Upper face is rotated clockwise
+    cube[1] = list(np.concatenate(np.rot90(bottom_face, rotations)))  # Bottom face is rotated anti-clockwise
+
     current_front_face = front_face
-
-
 
 
 def move_right_face(front_face):
@@ -279,25 +292,53 @@ def move_upper_face(front_face):
     cube[rotating_face][8] = old_cube[rotating_face][2]
 
 
-def move_right_inverse(front_face):
+def move_left_face(front_face):
+    set_front_face(get_back_face(front_face))
+    move_right_face(front_face)
+    set_front_face(front_face)
+
+
+def move_front_face(front_face):
+    set_front_face(get_left_face(front_face))
+    move_right_face(front_face)
+    set_front_face(front_face)
+
+
+def move_right_face_inverse(front_face):
     """simply move the cube 3 right moves and that's its inverse """
     move_right_face(front_face)
     move_right_face(front_face)
     move_right_face(front_face)
+
 
 def move_upper_face_inverse(front_face):
     move_upper_face(front_face)
     move_upper_face(front_face)
     move_upper_face(front_face)
 
+def move_left_face_inverse(front_face):
+    move_right_face(front_face)
+    move_right_face(front_face)
+    move_right_face(front_face)
+
+def move_front_face_inverse(front_face):
+    move_front_face(front_face)
+    move_front_face(front_face)
+    move_front_face(front_face)
 
 def perform_right_hand_rule(front_face):
     move_right_face(front_face)
     move_upper_face(front_face)
-    move_right_inverse(front_face)
+    move_right_face_inverse(front_face)
     move_upper_face_inverse(front_face)
 
-def append_images(images, direction='horizontal',  bg_color=(255, 255, 255), aligment='center'):
+def perform_left_hand_rule(front_face):
+    move_left_face_inverse(front_face)
+    move_upper_face_inverse(front_face)
+    move_left_face(front_face)
+    move_upper_face(front_face)
+
+def append_images(images, direction='horizontal', bg_color=(255, 255, 255), aligment='center'):
     """
     Appends images in horizontal/vertical direction.
 
@@ -355,9 +396,12 @@ def draw_cube(front_face):
     ]
     face_images = []
     for face in cube:
-        face_top_img = append_images([colors[faces.index(face[0])], colors[faces.index(face[1])], colors[faces.index(face[2])]])
-        face_middle_img = append_images([colors[faces.index(face[3])], colors[faces.index(face[4])], colors[faces.index(face[5])]])
-        face_bottom_img = append_images([colors[faces.index(face[6])], colors[faces.index(face[7])], colors[faces.index(face[8])]])
+        face_top_img = append_images(
+            [colors[faces.index(face[0])], colors[faces.index(face[1])], colors[faces.index(face[2])]])
+        face_middle_img = append_images(
+            [colors[faces.index(face[3])], colors[faces.index(face[4])], colors[faces.index(face[5])]])
+        face_bottom_img = append_images(
+            [colors[faces.index(face[6])], colors[faces.index(face[7])], colors[faces.index(face[8])]])
 
         face_img = append_images([face_top_img, face_middle_img, face_bottom_img], direction='vertical')
         face_images.append(face_img)
@@ -366,13 +410,13 @@ def draw_cube(front_face):
     padding_img = append_images([black_img, black_img, black_img])
     padding_img = append_images([padding_img, padding_img, padding_img], direction='vertical')
 
-    vertical_cube_img = append_images([face_images[get_back_face(front_face)],face_images[0],face_images[front_face],
+    vertical_cube_img = append_images([face_images[get_back_face(front_face)], face_images[0], face_images[front_face],
                                        face_images[1]], direction='vertical')
-    left_cube_img = append_images([padding_img,  face_images[get_left_face(front_face)]], direction='vertical')
-    right_cube_img = append_images([padding_img,  face_images[get_right_face(front_face)]], direction='vertical')
+    left_cube_img = append_images([padding_img, face_images[get_left_face(front_face)]], direction='vertical')
+    right_cube_img = append_images([padding_img, face_images[get_right_face(front_face)]], direction='vertical')
     # add_left_cube_img = append_images([padding_img, face_images[get_left_face(front_face)], vertical_cube_img], aligment='top')
     # add_right_cube_img = append_images([padding_img, add_left_cube_img,face_images[get_right_face(front_face)]], aligment='top')
-    cube_img = append_images([left_cube_img,vertical_cube_img,right_cube_img], bg_color=(0,0,0))
+    cube_img = append_images([left_cube_img, vertical_cube_img, right_cube_img], bg_color=(0, 0, 0))
     cube_img.show('foo')
 
 
@@ -388,7 +432,6 @@ def print_cube():
         print(face)
     draw_cube(4)
     print('\n\n')
-
 
 # initial_cube = copy.deepcopy(cube)
 #
@@ -411,5 +454,4 @@ def print_cube():
 # print('-----------------')
 # for face in cube:
 #     print(face)
-
 
